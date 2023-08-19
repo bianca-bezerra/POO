4fa52204c4b981d1f5ed4f20c30903d8f5ff1c55
bianca-bezerra <biancabezerrathe71@gmail.com>
2023-08-19 17:39:30 -0300

    Organização das questões do exercício 2

--- Exercicio_02/questao3.js
+++ Exercicio_02/questao3.js
@@ -10,4 +10,5 @@ console.log(typeof(resultado)) // string
 /* Devido à tipagem dinâmica do JavaScript, ao somar valores de tipos diferentes, o número 5 é 
 convertido em uma string para que possa ser concatenado com a string "10". O resultado impresso
 será a concatenação das duas strings, ou seja, "510". Essa conversão implícita pode levar a 
-resultados inesperados, como no exemplo, onde esperávamos uma soma aritmética. */
+resultados inesperados, como no exemplo, onde esperávamos uma soma aritmética.
+*/
